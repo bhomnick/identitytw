d389acd5f6907068da31aea904e33ba3c4353a57
Tom Fifield <tom@tomfifield.net>
2020-12-30 11:09:09 +0800

    Add a list of valid IDs to test with.
    These IDs have been previously released in public, found online.

--- identity/common/tests.py
+++ identity/common/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
+VALID_IDS = ['Z256783650', 'AB12345677', 'AC12345679', 'AB00207171', 'AC03095424',
+             'BD01300667', 'CC00151114', 'HD02717288', 'TD00251124']
